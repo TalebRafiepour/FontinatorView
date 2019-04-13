befd51846f7ab4998f46d112b81c62004b765c8b
Taleb <taleb.r75@gmail.com>
2019-04-13 17:38:35 +0430

    first commit

---
+++ fontview/src/main/java/com/taleb/fontview/FontEditText.kt
@@ -0,0 +1,32 @@
+package com.taleb.fontview
+
+import android.content.Context
+import android.graphics.Typeface
+import android.support.v7.widget.AppCompatEditText
+import android.util.AttributeSet
+
+class FontEditText: AppCompatEditText {
+
+    constructor(context: Context) : super(context){
+        init(context,null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init(context,attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context,attrs)
+    }
+
+    private fun init(context: Context, attributeSet: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.FontinatorView)
+        try {
+            val fontSrc = typedArray.getString(R.styleable.FontinatorView_font_asset_src)
+            typeface = Typeface.createFromAsset(context.assets, fontSrc)
+        } catch (ex: Exception) {
+        } finally {
+            typedArray.recycle()
+        }
+    }
+}
