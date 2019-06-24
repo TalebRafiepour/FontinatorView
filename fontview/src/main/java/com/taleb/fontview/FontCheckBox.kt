b1436ed0983712e1e902b33fe06bac1e98cd6f19
taleb <taleb.r75@gmail.com>
2019-06-24 15:55:13 +0430

    FontCheckBox added

---
+++ fontview/src/main/java/com/taleb/fontview/FontCheckBox.kt
@@ -0,0 +1,32 @@
+package com.taleb.fontview
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.CheckBox
+
+class FontCheckBox: CheckBox {
+
+    constructor(context: Context) : super(context){
+        init(context,null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs){
+        init(context,attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init(context,attrs)
+    }
+
+    private fun init(context: Context, attributeSet: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.FontinatorView)
+        try {
+            val fontSrc = typedArray.getString(R.styleable.FontinatorView_font_asset_src)
+            typeface = Typeface.createFromAsset(context.assets, fontSrc)
+        } catch (ex: Exception) {
+        } finally {
+            typedArray.recycle()
+        }
+    }
+}
